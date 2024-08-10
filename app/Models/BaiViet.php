916d3fb957103fc31f5d370b9842fe64ec142a32
sinhduong <sinhduong1508@gmail.com>
2024-08-10 16:12:08 +0700

    api

---
+++ app/Models/BaiViet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaiViet extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'hinh_anh',
+        'tieu_de',
+        'noi_dung',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ApiBaiVietController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/bai-viets',ApiBaiVietController::class);
+// Route::resource('bai-viets',[ApiBaiVietController::class]);
